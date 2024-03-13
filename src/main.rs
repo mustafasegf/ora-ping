a3e89dece760b212b83578c60d67617f7e44a4c9
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2024-03-13 22:08:17 +0700

    feat: not working implementation static file serve

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,13 @@ impl ProxyHttp for GW {
         ()
     }
 
-    async fn upstream_peer(&self, session: &mut Session, _ctx: &mut ()) -> Result<Box<HttpPeer>> {
+    async fn request_filter(&self, session: &mut Session, _ctx: &mut ()) -> Result<bool> {
+        // // if read is false, then connection is closed
+        // if session.read_request().await? == false {
+        //     println!("read_request: false");
+        //     return Ok(false);
+        // }
+
         let hostname = session
             .req_header()
             .headers
@@ -20,7 +26,6 @@ impl ProxyHttp for GW {
             .map_or("", |x| x.to_str().unwrap_or_default());
 
         let domain = "localhost:6188";
-
         let subdomain = match hostname.ends_with(domain) {
             true => hostname.trim_end_matches(domain),
             false => {
@@ -30,14 +35,115 @@ impl ProxyHttp for GW {
                 ))
             }
         };
+
+        if subdomain.is_empty() {
+            let base = "./static";
+
+            let method = &session.req_header().method;
+            if method != http::Method::GET && method != http::Method::HEAD {
+                return Err(Error::explain(
+                    HTTPStatus(StatusCode::METHOD_NOT_ALLOWED.into()),
+                    "method not allowed",
+                ));
+            }
+
+            let path_to_file = format!("{}{}", base, session.req_header().uri.path());
+            let path_to_file = if path_to_file.ends_with("/") {
+                format!("{}index.html", path_to_file)
+            } else {
+                path_to_file
+            };
+
+            println!("path_to_file: {}", path_to_file);
+
+            let file = match std::fs::read(&path_to_file) {
+                Ok(file) => file,
+                Err(_) => {
+                    return Err(Error::explain(
+                        HTTPStatus(StatusCode::NOT_FOUND.into()),
+                        "file not found",
+                    ))
+                }
+            };
+
+            println!("file: {:?}", file);
+
+            session.write_response_body(file.into()).await?;
+            session.finish_body().await?;
+
+            println!("writen: {:?}", session.response_written());
+            return Ok(true);
+        }
+
+        Ok(false)
+    }
+
+    async fn upstream_peer(&self, session: &mut Session, _ctx: &mut ()) -> Result<Box<HttpPeer>> {
+        let hostname = session
+            .req_header()
+            .headers
+            .get(header::HOST)
+            .map_or("", |x| x.to_str().unwrap_or_default());
+
+        let domain = "localhost:6188";
+        let subdomain = match hostname.ends_with(domain) {
+            true => hostname.trim_end_matches(domain),
+            false => {
+                return Err(Error::explain(
+                    HTTPStatus(StatusCode::BAD_REQUEST.into()),
+                    "invalid domain",
+                ))
+            }
+        };
+
         println!("subdomain: {}", subdomain);
         let localhost = "127.0.0.1".to_string();
 
         let addr = match subdomain {
-            "" => {
-                // serve static files. idk how
-                (localhost, 3000)
-            }
+            // "" => {
+            //     // serve static files
+            //     let base = "./static";
+            //
+            //     // check the request method
+            //     let method = &session.req_header().method;
+            //     if method != http::Method::GET && method != http::Method::HEAD {
+            //         return Err(Error::explain(
+            //             HTTPStatus(StatusCode::METHOD_NOT_ALLOWED.into()),
+            //             "method not allowed",
+            //         ));
+            //     }
+            //
+            //     // print cwd
+            //     let cwd = std::env::current_dir().unwrap();
+            //     println!("cwd: {:?}", cwd);
+            //
+            //     let path_to_file = format!("{}{}", base, session.req_header().uri.path());
+            //     let path_to_file = if path_to_file.ends_with("/") {
+            //         format!("{}index.html", path_to_file)
+            //     } else {
+            //         path_to_file
+            //     };
+            //
+            //     println!("path_to_file: {}", path_to_file);
+            //
+            //     let file = match std::fs::read(&path_to_file) {
+            //         Ok(file) => file,
+            //         Err(_) => {
+            //             return Err(Error::explain(
+            //                 HTTPStatus(StatusCode::NOT_FOUND.into()),
+            //                 "file not found",
+            //             ))
+            //         }
+            //     };
+            //
+            //     session.write_response_body(file.into()).await?;
+            //     session.finish_body().await?;
+            //
+            //     return Err(Error::explain(
+            //         HTTPStatus(StatusCode::OK.into()),
+            //         "static file served",
+            //     ));
+            // }
             "crafty" => (localhost, 8443),
             "short" => (format!("s.{domain}"), 443),
             "s" => (localhost, 8001),
@@ -56,7 +162,7 @@ impl ProxyHttp for GW {
             }
         };
 
-        let peer = Box::new(HttpPeer::new(addr, false, "".to_string()));
+        let peer = Box::new(HttpPeer::new(addr, false, domain.to_string()));
         Ok(peer)
     }
 }
