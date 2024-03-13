56ebc495dfce260098b85e87cd573857f140f6b9
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2024-03-14 01:24:57 +0700

    feat: move out subdomain to ctx

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,21 @@ use pingora_core::protocols::http::SERVER_NAME;
 use pingora_http::ResponseHeader;
 use std::sync::Arc;
 
+pub struct Context {
+    pub subdomain: String,
+}
+
 pub struct GW;
 #[async_trait]
 impl ProxyHttp for GW {
-    type CTX = ();
-    fn new_ctx(&self) -> () {
-        ()
+    type CTX = Context;
+    fn new_ctx(&self) -> Self::CTX {
+        Context {
+            subdomain: String::new(),
+        }
     }
 
-    async fn request_filter(&self, session: &mut Session, _ctx: &mut ()) -> Result<bool> {
+    async fn request_filter(&self, session: &mut Session, ctx: &mut Self::CTX) -> Result<bool> {
         let hostname = session
             .req_header()
             .headers
@@ -23,7 +29,7 @@ impl ProxyHttp for GW {
 
         let domain = "localhost:6188";
         let subdomain = match hostname.ends_with(domain) {
-            true => hostname.trim_end_matches(domain),
+            true => hostname.trim_end_matches(domain).trim_end_matches('.'),
             false => {
                 return Err(Error::explain(
                     HTTPStatus(StatusCode::BAD_REQUEST.into()),
@@ -31,6 +37,8 @@ impl ProxyHttp for GW {
                 ))
             }
         };
+
+        ctx.subdomain = subdomain.to_string();
 
         if subdomain.is_empty() {
             let base = "./static";
@@ -44,7 +52,7 @@ impl ProxyHttp for GW {
             }
 
             let path_to_file = format!("{}{}", base, session.req_header().uri.path());
-            let path_to_file = if path_to_file.ends_with("/") {
+            let path_to_file = if path_to_file.ends_with('/') {
                 format!("{}index.html", path_to_file)
             } else {
                 path_to_file
@@ -82,72 +90,18 @@ impl ProxyHttp for GW {
         Ok(false)
     }
 
-    async fn upstream_peer(&self, session: &mut Session, _ctx: &mut ()) -> Result<Box<HttpPeer>> {
-        let hostname = session
-            .req_header()
-            .headers
-            .get(header::HOST)
-            .map_or("", |x| x.to_str().unwrap_or_default());
-
+    async fn upstream_peer(
+        &self,
+        _session: &mut Session,
+        ctx: &mut Self::CTX,
+    ) -> Result<Box<HttpPeer>> {
         let domain = "localhost:6188";
-        let subdomain = match hostname.ends_with(domain) {
-            true => hostname.trim_end_matches(domain),
-            false => {
-                return Err(Error::explain(
-                    HTTPStatus(StatusCode::BAD_REQUEST.into()),
-                    "invalid domain",
-                ))
-            }
-        };
 
-        println!("subdomain: {}", subdomain);
         let localhost = "127.0.0.1".to_string();
 
-        let addr = match subdomain {
-            // "" => {
-            //     // serve static files
-            //     let base = "./static";
-            //
-            //     // check the request method
-            //     let method = &session.req_header().method;
-            //     if method != http::Method::GET && method != http::Method::HEAD {
-            //         return Err(Error::explain(
-            //             HTTPStatus(StatusCode::METHOD_NOT_ALLOWED.into()),
-            //             "method not allowed",
-            //         ));
-            //     }
-            //
-            //     // print cwd
-            //     let cwd = std::env::current_dir().unwrap();
-            //     println!("cwd: {:?}", cwd);
-            //
-            //     let path_to_file = format!("{}{}", base, session.req_header().uri.path());
-            //     let path_to_file = if path_to_file.ends_with("/") {
-            //         format!("{}index.html", path_to_file)
-            //     } else {
-            //         path_to_file
-            //     };
-            //
-            //     println!("path_to_file: {}", path_to_file);
-            //
-            //     let file = match std::fs::read(&path_to_file) {
-            //         Ok(file) => file,
-            //         Err(_) => {
-            //             return Err(Error::explain(
-            //                 HTTPStatus(StatusCode::NOT_FOUND.into()),
-            //                 "file not found",
-            //             ))
-            //         }
-            //     };
-            //
-            //     session.write_response_body(file.into()).await?;
-            //     session.finish_body().await?;
-            //
-            //     return Err(Error::explain(
-            //         HTTPStatus(StatusCode::OK.into()),
-            //         "static file served",
-            //     ));
-            // }
+        println!("subdomain: {}", ctx.subdomain);
+
+        let addr = match ctx.subdomain.as_str() {
             "crafty" => (localhost, 8443),
             "short" => (format!("s.{domain}"), 443),
             "s" => (localhost, 8001),
@@ -158,6 +112,7 @@ impl ProxyHttp for GW {
             "hahaha" => (localhost, 8086),
             "blog" => (localhost, 4321),
             "odoo" => (localhost, 8069),
+            "local" => (localhost, 3000),
             _ => {
                 return Err(Error::explain(
                     HTTPStatus(StatusCode::NOT_FOUND.into()),
